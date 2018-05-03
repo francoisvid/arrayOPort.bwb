01009fb3877f348b131db8eec96e1d7d024082cd
Vidal Francois <vidalfrancois11@gmail.com>
2018-05-03 03:09:43 +0200

    ennoncé complet

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
 /**
-* Decouverte de git 
-*
-*
+* Decouverte de php et de git a travers l'utilisation des fonctionnalités de php
+* http://php.net/manual/fr/reserved.variables.files.php
 */
+
+// opérations basiques sur les fichiers (les dossiers)
+
+// afficher le chemin complet du script
+
+// afficher les fichiers et les dossiers du dossier "serveurweb" dans une arborescence.
